555ae53a6ab3e33abff44c20e2fd2285a2ef5873
U-DESKTOP-QJSC0PK\PC <dstar55@yahoo.com>
2018-02-03 23:39:52 +0100

    #1 bootstrap.yml initial commit

--- services/hello-service/src/main/java/io/fractus/microstart/service/hello/Application.java
+++ services/hello-service/src/main/java/io/fractus/microstart/service/hello/Application.java
@@ -12,10 +12,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableEurekaClient
 public class Application {
     
     public static void main(String[] args) {
