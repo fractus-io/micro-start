14b6695994baec0699a07c9068a50c7513798ef5
U-DESKTOP-QJSC0PK\PC <dstar55@yahoo.com>
2018-02-10 14:32:19 +0100

    #3 moved from yml to properties file, basic configuration for hello service

--- services/hello-service/src/main/java/io/fractus/microstart/services/hello/HelloController.java
+++ services/hello-service/src/main/java/io/fractus/microstart/services/hello/HelloController.java
@@ -7,14 +7,21 @@ package io.fractus.microstart.services.hello;
 
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @RestController
 public class HelloController {
     
+    @Value("${message:Hello default}")
+    private String message;
+    
+    
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World\n";
+        return this.message;
     }
     
 }
