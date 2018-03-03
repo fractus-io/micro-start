be14ccbad81ecc07df38054574fa4f7f3a3892f7
U-DESKTOP-QJSC0PK\PC <dstar55@yahoo.com>
2018-03-03 20:42:10 +0100

    #7 hello-service Dockerfile updated

--- services/hello-service/src/main/docker/config/run.sh
+++ services/hello-service/src/main/docker/config/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # LOG CONFIGURATION
-sed -i s/LOGSTASH_HOST/$LOGSTASH_HOST/g  /opt/config/logback.xml
-sed -i s/LOGGING_APPLICATION_NAME/$LOGGING_APPLICATION_NAME/g  /opt/config/logback.xml
+#sed -i s/LOGSTASH_HOST/$LOGSTASH_HOST/g  /opt/config/logback.xml
+#sed -i s/LOGGING_APPLICATION_NAME/$LOGGING_APPLICATION_NAME/g  /opt/config/logback.xml
 
 cat /opt/config/logback.xml
 
@@ -17,15 +17,16 @@ echo server.port=8080 >> /opt/config/application.properties
 echo management.port=8080 >> /opt/config/application.properties
 
 # a service application default port
-echo server.port=$SERVER_PORT >> /opt/config/application.properties
+#echo server.port=$SERVER_PORT >> /opt/config/application.properties
 
 # a Service Id (sid) is a unique (micro)service instance identificator 
 # echo sid=$SID >> /opt/config/application.properties
 
-echo spring.profiles.active=$SPRING_PROFILES >> /opt/config/application.properties
+#echo spring.profiles.active=$SPRING_PROFILES >> /opt/config/application.properties
 
 cat /opt/config/application.properties
 
 # start spring boot application
-exec /usr/bin/java $JVM_MAX_MEM $JVM_PARAMS  -jar /opt/*hello-service*.jar --spring.config.location=/opt/config/application.properties
+#exec /usr/bin/java $JVM_MAX_MEM $JVM_PARAMS  -jar /opt/*hello-service*.jar --spring.config.location=/opt/config/application.properties
 
+exec /usr/bin/java -jar /opt/*hello-service*.jar 
