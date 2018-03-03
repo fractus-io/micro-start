fd147053658ff289c28762cf42c6310443b1597e
U-DESKTOP-QJSC0PK\PC <dstar55@yahoo.com>
2018-03-03 20:50:01 +0100

    #7 hello-service Dockerfile updated

--- services/hello-service/src/main/docker/config/run.sh
+++ services/hello-service/src/main/docker/config/run.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # LOG CONFIGURATION
-#sed -i s/LOGSTASH_HOST/$LOGSTASH_HOST/g  /opt/config/logback.xml
-#sed -i s/LOGGING_APPLICATION_NAME/$LOGGING_APPLICATION_NAME/g  /opt/config/logback.xml
+#sed -i s/LOGSTASH_HOST/$LOGSTASH_HOST/g  /opt/logback.xml
+#sed -i s/LOGGING_APPLICATION_NAME/$LOGGING_APPLICATION_NAME/g  /opt/logback.xml
 
-cat /opt/config/logback.xml
+cat /opt/logback.xml
 
 # APPLICATION PROPERTIES
-touch /opt/config/application.properties
+touch /opt/application.properties
 
 # GLOBALS
-echo logging.config=/opt/config/logback.xml >> /opt/config/application.properties
+echo logging.config=/opt/logback.xml >> /opt/application.properties
 
-echo shell.ssh.port=2022 >> /opt/config/application.properties
-echo server.port=8080 >> /opt/config/application.properties
-echo management.port=8080 >> /opt/config/application.properties
+echo shell.ssh.port=2022 >> /opt/application.properties
+echo server.port=8080 >> /opt/application.properties
+echo management.port=8080 >> /opt/application.properties
 
 # a service application default port
-#echo server.port=$SERVER_PORT >> /opt/config/application.properties
+#echo server.port=$SERVER_PORT >> /opt/application.properties
 
 # a Service Id (sid) is a unique (micro)service instance identificator 
-# echo sid=$SID >> /opt/config/application.properties
+# echo sid=$SID >> /opt/application.properties
 
-#echo spring.profiles.active=$SPRING_PROFILES >> /opt/config/application.properties
+#echo spring.profiles.active=$SPRING_PROFILES >> /opt/application.properties
 
-cat /opt/config/application.properties
+cat /opt/application.properties
 
 # start spring boot application
-#exec /usr/bin/java $JVM_MAX_MEM $JVM_PARAMS  -jar /opt/*hello-service*.jar --spring.config.location=/opt/config/application.properties
+#exec /usr/bin/java $JVM_MAX_MEM $JVM_PARAMS  -jar /opt/*hello-service*.jar --spring.config.location=/opt/application.properties
 
 exec /usr/bin/java -jar /opt/*hello-service*.jar 
